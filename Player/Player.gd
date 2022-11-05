748951aa3ad8da1fcf1580503b0ad0016fa7895f
mathat13 <lovell.jwz@gmail.com>
2022-11-05 20:06:12 +0000

    Player Image added.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0,0)
+onready var sprite = $Sprite
 
+var velocity = Vector2(0,0)
 export var accel:float = 6.0
 export var max_velocity = 300
 
+func change_direction(flip:bool):
+	sprite.flip_h = flip if sprite.flip_h != flip else sprite.flip_h
 
 func get_movement_vector():
 	var move_vector = Vector2.ZERO
@@ -19,4 +22,6 @@ func _physics_process(_delta):
 	velocity.x += clamp(desired_x, -accel, accel)
 	velocity.y += clamp(desired_y, -accel, accel)
 	print(accel, velocity)
+	change_direction(velocity.x >= 0)
 	velocity = move_and_slide(velocity)
+	
